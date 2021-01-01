f44fcd9159d3cf8f36aa448805358cc365d6d641
victorDev <victor01.santos@catolicasc.org.br>
2021-01-01 18:21:51 -0300

    Application Base

---
+++ src/main/java/com/victor/cursohibernate/CursohibernateApplication.java
@@ -0,0 +1,15 @@
+package com.victor.cursohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursohibernateApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CursohibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/cursohibernate/CursohibernateApplicationTests.java
@@ -0,0 +1,15 @@
+package com.victor.cursohibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursohibernateApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}

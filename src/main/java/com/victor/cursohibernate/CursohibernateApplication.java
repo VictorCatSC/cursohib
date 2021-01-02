97690f0267b077230a0d1b4022eed1f2f56612e1
victorDev <victor01.santos@catolicasc.org.br>
2021-01-02 18:14:16 -0300

    'Estado' & 'Cidade' domain (models)

--- src/main/java/com/victor/cursohibernate/CursohibernateApplication.java
+++ src/main/java/com/victor/cursohibernate/CursohibernateApplication.java
@@ -1,8 +1,12 @@
 package com.victor.cursohibernate;
 
 import com.victor.cursohibernate.domain.Categoria;
+import com.victor.cursohibernate.domain.Cidade;
+import com.victor.cursohibernate.domain.Estado;
 import com.victor.cursohibernate.domain.Produto;
 import com.victor.cursohibernate.repositoriesDAO.CategoriaRepository;
+import com.victor.cursohibernate.repositoriesDAO.CidadeRepository;
+import com.victor.cursohibernate.repositoriesDAO.EstadoRepository;
 import com.victor.cursohibernate.repositoriesDAO.ProdutoRepository;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,12 @@ public class CursohibernateApplication implements CommandLineRunner
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(CursohibernateApplication.class, args);
@@ -35,6 +45,7 @@ public class CursohibernateApplication implements CommandLineRunner
 		Produto p2 = new Produto(null, "impressora", 800.0);
 		Produto p3 = new Produto(null, "mouse", 80.0);
 
+		//substituir getProdutos().addAll(...) por setProdutos(...)
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -44,5 +55,19 @@ public class CursohibernateApplication implements CommandLineRunner
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.setCidades(Arrays.asList(c1));
+		est2.setCidades(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 }

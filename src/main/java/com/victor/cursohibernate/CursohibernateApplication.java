703c745d2a2cd13f42709cbc27a1d2b0f75a1dcb
victorDev <victor01.santos@catolicasc.org.br>
2021-01-06 20:46:35 -0300

    endpoint paginação categorias

--- src/main/java/com/victor/cursohibernate/CursohibernateApplication.java
+++ src/main/java/com/victor/cursohibernate/CursohibernateApplication.java
@@ -55,6 +55,11 @@ public class CursohibernateApplication implements CommandLineRunner
 	{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama Mesa e Banho");
+		Categoria cat4 = new Categoria(null, "Esportes");
+		Categoria cat5 = new Categoria(null, "Saude");
+		Categoria cat6 = new Categoria(null, "Escola");
+		Categoria cat7 = new Categoria(null, "Limpeza");
 
 		Produto p1 = new Produto(null, "computador", 2000.0);
 		Produto p2 = new Produto(null, "impressora", 800.0);
@@ -68,7 +73,7 @@ public class CursohibernateApplication implements CommandLineRunner
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/victor/cursohibernate/resources/CategoriaResource.java
+++ src/main/java/com/victor/cursohibernate/resources/CategoriaResource.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -61,4 +62,18 @@ public class CategoriaResource
 
 		return ResponseEntity.ok().body(dtoObjList);
 	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		@RequestParam(value = "page", defaultValue = "0") Integer page,
+		@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+		@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+		@RequestParam(value = "direction", defaultValue = "ASC") String direction)
+	{
+		Page<Categoria> objList = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> dtoObjList = objList.map(obj -> new CategoriaDTO(obj));
+
+		return ResponseEntity.ok().body(dtoObjList);
+	}
+
 }

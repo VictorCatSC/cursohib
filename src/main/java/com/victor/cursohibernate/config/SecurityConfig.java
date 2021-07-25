ef93437d3dc283673ef31a987438513e3cabf3d1
victor santos <victor.santos@neomind.com.br>
2021-07-25 10:46:26 -0300

    jwt

--- src/main/java/com/victor/cursohibernate/config/SecurityConfig.java
+++ src/main/java/com/victor/cursohibernate/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.victor.cursohibernate.config;
 
+import com.victor.cursohibernate.security.JWTAuthenticationFilter;
+import com.victor.cursohibernate.security.JWTUtil;
+import com.victor.cursohibernate.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,6 +24,12 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @Autowired
     private Environment env;
@@ -46,7 +57,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
 
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
---
+++ src/main/java/com/victor/cursohibernate/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.victor.cursohibernate.services;
+
+import com.victor.cursohibernate.domain.Cliente;
+import com.victor.cursohibernate.repositoriesDAO.ClienteRepository;
+import com.victor.cursohibernate.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cliente = repo.findByEmail(email);
+
+        if(cliente == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+    }
+}
